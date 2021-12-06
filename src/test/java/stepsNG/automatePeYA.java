cf7151614b1ce278a662349009093df20b6bd70f
ferreiracamilo <camiloferreira2009@hotmail.com>
2021-12-06 00:01:17 -0300

    Notes within autoatePeYa java class about what will be implemented to execute applying testng

--- src/test/java/stepsNG/automatePeYA.java
+++ src/test/java/stepsNG/automatePeYA.java
@@ -7,5 +7,50 @@ public class automatePeYA extends BasePage {
     //Even though 'stepsNG'is not an appropiate name to assign to TestNG classes package, anyways to indicate easily name was given like that.
     //On this case that is required to execute certain actions from a document received, this file will only one to execute test actions
 
+    /*
+    Original actions to do
+
+    Scenario to be automated:
+    Go to https://www.pedidosya.com.uy/
+    Set any street and search for “Pizza”
+    Click Search button (Confirm the street, do not move the location)
+    Report total no. of Search results with no. of results in the current page
+    Select a random Filter (Express, Sellos, etc)
+    Verify the number of results is correct
+    Sort by: “Alfabeticamente”
+    Verify the sort is correct
+    Report total no. of Search results with no. of results in the current page
+    Report the star rating of each of the results in the first result page
+    Go into the first results from the search results
+    Log all critical information of the selected restaurant:
+    Details (distance, open hours, rating points, etc)
+    First 3 customer reviews (name, date, rating points, etc)
+
+    Bonus: Run them on firefox too
+    Note: For this scenario cucumber best practices can be ignored
+
+    * */
+
+    /*
+    ACTIONS TO BE DONE AFTER SOME MODIFICATION DUE TO SAVE EFFORT OR NOT APPLYING ANYMORE
+    Scenario to be automated:
+    Go to https://www.pedidosya.com.uy/
+    Set any street and search for “Pizza”
+    Click Search button (Confirm the street, do not move the location)
+    Report total no. of Search results with no. of results in the current page
+    Select a random Filter (Express, Sellos, etc)
+    Verify the number of results is correct
+    Report the star rating and name of first 5 restaurants
+    Go into the first results from the search results
+    Log all critical information of the selected restaurant:
+    Details (distance, open hours, rating points, etc)
+    First 3 customer reviews (name, date, rating points, etc)
+
+    Bonus: Run them on firefox too
+    Note: Finally was applied TestNG
+
+    * */
+
+
 
 }
